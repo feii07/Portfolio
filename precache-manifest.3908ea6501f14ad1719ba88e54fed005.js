c6a7032350ed17b04f394dceced55cfeb438b3bd
unknown <christy.ferani@gmail.com>
2023-10-29 16:38:20 +0700

    Updates

---
+++ precache-manifest.3908ea6501f14ad1719ba88e54fed005.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1208706c24a25fc8376dbf05dbf5c1ae",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "79f94d9b1ea895acbd01",
+    "url": "/portfolio/static/css/main.ad58301f.chunk.css"
+  },
+  {
+    "revision": "58f17d6be0cc67638f20",
+    "url": "/portfolio/static/js/2.e994857e.chunk.js"
+  },
+  {
+    "revision": "77da778a10b57eb72fd7",
+    "url": "/portfolio/static/js/3.5d65379c.chunk.js"
+  },
+  {
+    "revision": "79f94d9b1ea895acbd01",
+    "url": "/portfolio/static/js/main.e4112073.chunk.js"
+  },
+  {
+    "revision": "bce3b14d98c42d308c83",
+    "url": "/portfolio/static/js/runtime~main.3a7c4aaf.js"
+  }
+]);
